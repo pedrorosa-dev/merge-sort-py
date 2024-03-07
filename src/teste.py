f72af36a20ba7587e97217a9a91f9fc302b0315c
pedrorosa-dev <phrosa.c@gmail.com>
2024-03-07 18:59:05 -0300

    rafa dengo

--- src/teste.py
+++ src/teste.py
@@ -1,22 +1,23 @@
 import random as rd
 
 
-num = int(input("tente descobrir um número entre 0 e 100: "))
-shot = 1
+num = int(input("insira um número entre 0 e 100: "))
+shot = 0
 shots = []
+minimum = 0
+maximum = 100
+aleat = rd.randint(minimum, maximum)
 
-aleat = rd.randint(0, 100)
-
-while num != aleat:
-    if num > aleat:
-        print("menor")
-        num = int(input("tente descobrir um número entre 0 e 100: "))
+while (aleat != num):
+    if aleat > num:
+        maximum = aleat
+        aleat = rd.randint(minimum+1, maximum-1)
         shot += 1
-    elif num < aleat:
-        print("maior")
-        num = int(input("tente descobrir um número entre 0 e 100: "))
+    elif aleat < num:
+        minimum = aleat
+        aleat = rd.randint(minimum+1, maximum-1)
         shot += 1
-    shots.append(num)
+    shots.append(aleat)
 
-print("acertou o número em {} tentativas".format(shot))
-print("você tentou os seguintes numeros: {}".format(shots))
+print("o número foi acertado em {} tentativas".format(shot))
+print("a máquina tentou os seguintes numeros: {}".format(shots))
