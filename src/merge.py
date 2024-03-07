add675596b3ce229c404ca924f934ff81a140596
pedrorosa-dev <phrosa.c@gmail.com>
2024-03-07 19:06:36 -0300

    fix: ajeitar os nomes das variaveis

--- src/merge.py
+++ src/merge.py
@@ -1,7 +1,7 @@
 def merge_sort(array):
     if len(array) > 1:
-        meio = len(array) // 2
-        esquerda, direita = array[:meio], array[meio:]
-        merge_sort(esquerda)
-        merge_sort(direita)
-        array[:] = sorted(esquerda + direita)
+        ListaDoMeio = len(array) // 2
+        ListaDaEsquerda, ListaDaDireita = array[:ListaDoMeio], array[ListaDoMeio:]
+        merge_sort(ListaDaEsquerda)
+        merge_sort(ListaDaDireita)
+        array[:] = sorted(ListaDaEsquerda + ListaDaDireita)
--- src/user.py
+++ src/user.py
@@ -1,19 +1,20 @@
 from src.merge import merge_sort
 
+
 def main():
-    numeros = []
+    ListaDosNumeros = []
     while True:
         try:
             num = input("Digite um número (ou 's' para parar): ")
             if num.lower() == 's':
                 break
-            numeros.append(int(num))
+            ListaDosNumeros.append(int(num))
         except ValueError:
             print("Por favor, digite um número válido.")
 
-    if numeros:
-        print("Lista original:", numeros)
-        merge_sort(numeros)
-        print("Lista ordenada merge sort:", numeros)
+    if ListaDosNumeros:
+        print("Lista nao ordenada com merge sort:", ListaDosNumeros)
+        merge_sort(ListaDosNumeros)
+        print("Lista ordenada merge sort:", ListaDosNumeros)
     else:
         print("A lista está vazia.")
