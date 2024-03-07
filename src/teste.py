311b7914cb9bfa352c541d8c9a1cf7505eccaf65
pedrorosa-dev <phrosa.c@gmail.com>
2024-03-07 18:36:53 -0300

    rafa dengo

--- src/teste.py
+++ src/teste.py
@@ -1,1 +1,22 @@
-print("hello world")
+import random as rd
+
+
+num = int(input("tente descobrir um número entre 0 e 100: "))
+shot = 1
+shots = []
+
+aleat = rd.randint(0, 100)
+
+while num != aleat:
+    if num > aleat:
+        print("menor")
+        num = int(input("tente descobrir um número entre 0 e 100: "))
+        shot += 1
+    elif num < aleat:
+        print("maior")
+        num = int(input("tente descobrir um número entre 0 e 100: "))
+        shot += 1
+    shots.append(shot)
+
+print("acertou o número em {} tentativas".format(shot))
+print("você tentou os seguintes numeros: {}".format(shots))
